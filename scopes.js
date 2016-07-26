d72e078dc9b06e920377aa6a2815df1cd2f4787d
zhang <zhangtuo2008@gmail.com>
2016-07-26 02:58:55 +0200

    exo2 & 3

--- scopes.js
+++ scopes.js
@@ -2,6 +2,17 @@
  * Created by tuo on 16/7/26.
  */
 
+/*
+// exo1
 function foo() {
     var bar
+}*/
+
+// exo2 & 3
+function foo() {
+    var bar
+    quux = 2
+    function zip() {
+        var quux =1
+    }
 }
