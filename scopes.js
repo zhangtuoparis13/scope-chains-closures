1c9c31dc34a62683c9a15403bdf516d115930516
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-07-26 13:13:55 +0200

    exo 4  Very close. You're actually returning the result of zip when doing zip(); (it's being called), you want to return the actual function, so don't invoke it.

--- scopes.js
+++ scopes.js
@@ -2,17 +2,18 @@
  * Created by tuo on 16/7/26.
  */
 
-/*
 // exo1
-function foo() {
+/*function foo() {
     var bar
 }*/
 
 // exo2 & 3
 function foo() {
-    var bar
+    var bar = 2
     quux = 2
     function zip() {
         var quux =1
+        bar = true
     }
+    return zip
 }
